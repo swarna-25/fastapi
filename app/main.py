c2817476ede12c5e14e19f347f3b2fe5038a38e2
Swarna Priya <priyaswarna4@gmail.com>
2026-07-09 18:41:26 +0530

    some chnages

--- app/main.py
+++ app/main.py
@@ -43,7 +43,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World!!!!!"}
 
 # @app.get("/sqlalchemy")
 # def test_posts(db: Session=Depends(get_db)):
