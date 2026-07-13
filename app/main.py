c4ae64129da549db50f17302a93b1c104560eb51
Swarna Priya <priyaswarna4@gmail.com>
2026-07-14 00:09:38 +0530

    deploying to ubuntu

--- app/main.py
+++ app/main.py
@@ -43,7 +43,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World pushing out to ubuntu"}
 
 # @app.get("/sqlalchemy")
 # def test_posts(db: Session=Depends(get_db)):
